254c662448859b46b8376210a04846b794872d13
Alejandro F. Carrera <alejfcarrera@mail.ru>
2017-06-20 15:41:02 +0200

    created new project for geokettle-worker

---
+++ geokettle-worker/README.md
@@ -0,0 +1,7 @@
+## GeoKettle Worker
+
+---
+
+GeoLinkeddata © Copyright 2017.
+
+Alejandro F. Carrera
---
+++ geokettle-worker/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Ontology Engineering Group
+        http://www.oeg-upm.net/
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Copyright (C) 2017 Ontology Engineering Group.
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+            http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+#-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=#
+"""
+
+import os
+from setuptools import setup, find_packages
+
+__author__ = "Alejandro F. Carrera"
+__copyright__ = "Copyright 2017, GeoLinkeddata Platform"
+__credits__ = ["Alejandro F. Carrera", "Oscar Corcho"]
+__license__ = "Apache"
+__version__ = "2.0"
+__maintainer__ = "Alejandro F. Carrera"
+__email__ = "alejfcarrera@mail.ru"
+
+
+def read(name):
+    return open(os.path.join(os.path.dirname(__file__), name)).read()
+
+setup(
+    name="geokettle-worker",
+    version="1.0.0",
+    author=__author__,
+    author_email=__email__,
+    description="This software allows you execute jobs and transformations from asynchronous way",
+    license="Apache",
+    keywords="geolinkeddata celery worker geokettle gis",
+    url="https://github.com/oeg-upm/website-oeg.git",
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    install_requires=['celery'],
+    classifiers=[],
+    scripts=['geo_worker.py']
+)
